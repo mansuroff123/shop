a829655640adf3f63403b74b0114bf0fa86af6d4
mansuroff123 <mansuroff145@gmail.com>
2024-07-23 18:37:42 +0500

    test

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Product
 
 # Create your views here.
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'main/home.html'
+    context_object_name = 'products'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'product_detail.html'
+    context_object_name = 'product'
+
