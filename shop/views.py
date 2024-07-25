ac00904c6dfb199783d9863e741fa9d4d5115cb4
mansuroff123 <mansuroff145@gmail.com>
2024-07-25 17:31:03 +0500

    pagination

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.core.paginator import Paginator
 from .models import Product
 
 # Create your views here.
@@ -9,10 +10,11 @@ class ProductListView(ListView):
     model = Product
     template_name = 'main/home.html'
     context_object_name = 'products'
+    paginate_by = 1
 
 
 class ProductDetailView(DetailView):
     model = Product
-    template_name = 'product_detail.html'
+    template_name = 'main/product_detail.html'
     context_object_name = 'product'
 
