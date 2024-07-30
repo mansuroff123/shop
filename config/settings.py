97fad696302fc2156691b7724c48c5025c419278
mansuroff123 <mansuroff145@gmail.com>
2024-07-30 18:37:02 +0500

    auth section working

--- config/settings.py
+++ config/settings.py
@@ -20,7 +20,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',
+    'django.contrib.registration',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -34,7 +34,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.registration.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
@@ -50,7 +50,7 @@ TEMPLATES = [
             'context_processors': [
                 'django.template.context_processors.debug',
                 'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
+                'django.contrib.registration.context_processors.registration',
                 'django.contrib.messages.context_processors.messages',
             ],
         },
@@ -76,16 +76,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.registration.password_validation.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.registration.password_validation.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.registration.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.registration.password_validation.NumericPasswordValidator',
     },
 ]
 
--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.core.paginator import Paginator
 from .models import Product
+
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.shortcuts import render, redirect
+from .forms import SignUpForm, SignInForm
+from django.views.generic import CreateView
 
 # Create your views here.
 
@@ -18,3 +24,15 @@ class ProductDetailView(DetailView):
     template_name = 'main/product_detail.html'
     context_object_name = 'product'
 
+
+class SignUpView(CreateView):
+    form_class = SignUpForm
+    template_name = "registration/sign_up.html"
+    success_url = reverse_lazy('login')
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+class SignInView(LoginView):
+    form_class = SignInForm
+    template_name = "registration/sign_in.html"
