97b3312f44baa787b65f67d42ff50042a8d88f54
mansuroff123 <mansuroff145@gmail.com>
2024-07-30 19:14:14 +0500

    auth

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('shop.urls'))
+    path('', include('shop.urls')),
+    path('accounts/', include('shop.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- shop/urls.py
+++ shop/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import ProductDetailView, ProductListView
+from .views import SignUpView, SignInView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', SignInView.as_view(), name='login'),
 ]
+
+
