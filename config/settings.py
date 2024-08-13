882dcd0cf53728c377de516039491a85f015501b
mansuroff123 <mansuroff145@gmail.com>
2024-08-13 17:41:56 +0500

    404 page

--- config/settings.py
+++ config/settings.py
@@ -11,9 +11,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-yi(rc!d@dsmwr*=w))b2=mq*=b)e1caapvvb)u1n+(h)n-*6@t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -108,7 +108,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / "static"
 
 STATICFILES_DIRS = BASE_DIR / 'assets',
 
